1b97c839ee0eef0f284fb8ddab2d0938d9a2288a
Jorge3467 <jorgesalazar3467@gmail.com>
2024-02-20 15:57:39 -0500

    sistema de control de asistencia

---
+++ resources/views/usuarios/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<h1>Crear usuario</h1>
+
+@endsection
---
+++ resources/views/usuarios/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<h1>Lista de usuarios</h1>
+
+@endsection
